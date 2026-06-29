1ce87652681108fca6053b9be6decb90e50fc350
RishikeshPVijay <63793862+RishikeshPVijay@users.noreply.github.com>
2026-06-30 00:35:26 +0530

    feat(client): bootstrap client application (#7)
    closes #3 
    
    
    ### Changes
    
    - Bootstraped the client application
    - Modified root tsconfig and eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,32 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import { defineConfig } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+  globalIgnores(['**/dist', '**/coverage', 'pnpm-lock.yaml', 'pnpm-workspace.yaml']),
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-    ignores: ['coverage', '**/dist', 'pnpm-lock.yaml', 'pnpm-workspace.yaml'],
+    files: ['apps/server/**/*.ts'],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
-  tseslint.configs.recommended,
+
+  {
+    files: ['apps/web/**/*.{ts,tsx}'],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
   eslintConfigPrettier,
 ]);
