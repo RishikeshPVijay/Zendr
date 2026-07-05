a266e9d82cdebd69753327cb6d431d80b413202a
Rishikesh P Vijay <rishikeshpvijay@gmail.com>
2026-07-06 03:58:47 +0530

    feat(web): responsiveness

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,15 +1,22 @@
 import type { IconType } from 'react-icons';
 import { MdMonitor, MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { TbTransfer } from 'react-icons/tb';
+import { useResponsive } from '../hooks';
 import { useTheme } from '../providers/theme';
 import type { Theme } from '../types/app';
 import { SectionWrapper } from './SectionWrapper';
 
 export const Header = () => {
+  const { isMobile } = useResponsive();
+
   return (
     <SectionWrapper sectionClassName="bg-header shadow-header">
       <aside className="flex justify-between p-5">
         <span className="text-text-primary">Logo</span>
-        <ThemeButton />
+        <div className="flex gap-4">
+          {isMobile && <TbTransfer className="text-text-primary cursor-pointer text-2xl" />}
+          <ThemeButton />
+        </div>
       </aside>
     </SectionWrapper>
   );
