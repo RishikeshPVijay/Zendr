8d82c946942f4d0f1db3b614dc5df0ee275c8cb3
Rishikesh P Vijay <rishikeshpvijay@gmail.com>
2026-07-19 03:04:32 +0530

    refactor: error handling

--- apps/web/src/providers/websocket/provider.tsx
+++ apps/web/src/providers/websocket/provider.tsx
@@ -3,6 +3,12 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { WebSocketClient } from './client';
 import { WebSocketContext } from './context';
 
+const url = import.meta.env.VITE_WS_SERVER_URL;
+
+if (!url) {
+  throw new Error('Missing VITE_WS_SERVER_URL');
+}
+
 export const WebSocketProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const client = useMemo(() => new WebSocketClient(), []);
   const [connected, setConnected] = useState(false);
@@ -12,10 +18,16 @@ export const WebSocketProvider: React.FC<React.PropsWithChildren> = ({ children 
     const removeOpen = client.onOpen(() => setConnected(() => true));
     const removeClose = client.onClose(() => setConnected(() => false));
     const removeMessageListeners = client.onMessage((message) => {
-      messageHandlers.current.forEach((handler) => handler(message));
+      for (const handler of messageHandlers.current) {
+        try {
+          handler(message);
+        } catch (err) {
+          console.error(err);
+        }
+      }
     });
 
-    client.connect(import.meta.env.VITE_WS_SERVER_URL);
+    client.connect(url);
 
     return () => {
       removeOpen();
@@ -25,12 +37,7 @@ export const WebSocketProvider: React.FC<React.PropsWithChildren> = ({ children 
     };
   }, [client]);
 
-  const send = useCallback(
-    (message: BaseMessage) => {
-      client.send(message);
-    },
-    [client],
-  );
+  const send = useCallback((message: BaseMessage) => client.send(message), [client]);
 
   const addMessageHandler = useCallback((handler: (message: BaseMessage) => void) => {
     messageHandlers.current.add(handler);
